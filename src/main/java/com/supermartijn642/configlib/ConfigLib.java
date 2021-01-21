93d2913f0034d5ce6ae9e90ad192f9724690bc11
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-21 02:38:43 +0100

    Fix wrong modid and name Add a dependency on Config Lib to the example mod

--- src/main/java/com/supermartijn642/configlib/ConfigLib.java
+++ src/main/java/com/supermartijn642/configlib/ConfigLib.java
@@ -18,8 +18,8 @@ import java.util.*;
 @Mod(modid = ConfigLib.MODID, name = ConfigLib.NAME, version = ConfigLib.VERSION)
 public class ConfigLib {
 
-    public static final String MODID = "scarecrowsterritory";
-    public static final String NAME = "Scarecrow's Territory";
+    public static final String MODID = "supermartijn642configlib";
+    public static final String NAME = "SuperMartijn642's Config Library";
     public static final String VERSION = "1.0.0";
 
     private static final List<ModConfig> CONFIGS = new ArrayList<>();
--- src/test/java/ConfigLibExampleMod.java
+++ src/test/java/ConfigLibExampleMod.java
@@ -3,12 +3,13 @@ import net.minecraftforge.fml.common.Mod;
 /**
  * Created 1/21/2021 by SuperMartijn642
  */
-@Mod(modid = ConfigLibExampleMod.MODID, name = ConfigLibExampleMod.NAME, version = ConfigLibExampleMod.VERSION)
+@Mod(modid = ConfigLibExampleMod.MODID, name = ConfigLibExampleMod.NAME, version = ConfigLibExampleMod.VERSION, dependencies = ConfigLibExampleMod.DEPENDENCIES)
 public class ConfigLibExampleMod {
 
     public static final String MODID = "scarecrowsterritory";
     public static final String NAME = "Scarecrow's Territory";
     public static final String VERSION = "1.0.0";
+    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.0,)";
 
     public ConfigLibExampleMod(){
     }
