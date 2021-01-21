b32e50e87b66c481151fe82ff30a1867aee8109b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-21 02:25:13 +0100

    Add an example mod

---
+++ src/test/java/ConfigLibExampleMod.java
@@ -0,0 +1,12 @@
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created 1/21/2021 by SuperMartijn642
+ */
+@Mod("configlibexamplemod")
+public class ConfigLibExampleMod {
+
+    public ConfigLibExampleMod(){
+    }
+
+}
