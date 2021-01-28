824e059803c30f0b30b072e0ac3c6e6f81f1f3c8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-28 19:35:27 +0100

    update version

--- src/main/java/com/supermartijn642/configlib/ConfigLib.java
+++ src/main/java/com/supermartijn642/configlib/ConfigLib.java
@@ -21,7 +21,7 @@ public class ConfigLib {
 
     public static final String MODID = "supermartijn642configlib";
     public static final String NAME = "SuperMartijn642's Config Library";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     private static final List<ModConfig> CONFIGS = new ArrayList<>();
     private static final Map<ModConfig.Type,List<ModConfig>> CONFIGS_PER_TYPE = new EnumMap<>(ModConfig.Type.class);
