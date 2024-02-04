f87425debbaa47ffc74500d7494c916caeb15304
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-04 23:17:54 +0100

    Account for network changes in NeoForge 20.4.70

--- src/main/java/com/supermartijn642/configlib/ConfigLib.java
+++ src/main/java/com/supermartijn642/configlib/ConfigLib.java
@@ -4,6 +4,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.ModLoadingContext;
@@ -13,9 +14,8 @@ import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
-import net.neoforged.neoforge.network.NetworkRegistry;
 import net.neoforged.neoforge.network.PacketDistributor;
-import net.neoforged.neoforge.network.simple.SimpleChannel;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,15 +31,12 @@ public class ConfigLib {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("configlib");
 
-    protected static final ResourceLocation CHANNEL_ID = new ResourceLocation("supermartijn642configlib", "sync_configs");
-    private static SimpleChannel channel;
-
     private static final List<ModConfig<?>> CONFIGS = new ArrayList<>();
     private static final Set<String> CONFIG_NAMES = new HashSet<>();
     private static final List<ModConfig<?>> SYNCABLE_CONFIGS = new ArrayList<>();
     private static final Map<String,ModConfig<?>> SYNCABLE_CONFIGS_BY_IDENTIFIER = new HashMap<>();
 
-    public ConfigLib(){
+    public ConfigLib(IEventBus eventBus){
         // Allow connection if there are no syncable configs or if the server has the same mod version
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(ConfigLib::getModVersion, (remoteVersion, isFromServer) -> canConnectWith(remoteVersion)));
 
@@ -52,16 +49,16 @@ public class ConfigLib {
         if(isClientEnvironment())
             ConfigLibClient.registerEventListeners();
 
-        channel = NetworkRegistry.ChannelBuilder.named(CHANNEL_ID)
-            .networkProtocolVersion(ConfigLib::getModVersion)
-            .clientAcceptedVersions(ConfigLib::canConnectWith)
-            .serverAcceptedVersions(ConfigLib::canConnectWith)
-            .simpleChannel();
-        channel.messageBuilder(ConfigSyncPacket.class, 0)
-            .encoder(ConfigLib::createSyncedEntriesPacket)
-            .decoder(ConfigLib::handleSyncConfigPacket)
-            .consumerNetworkThread((packet, context) -> context.setPacketHandled(true))
-            .add();
+        eventBus.addListener((Consumer<RegisterPayloadHandlerEvent>)e ->
+            e.registrar("supermartijn642configlib")
+                .versioned(getModVersion())
+                .optional()
+                .configuration(
+                    new ResourceLocation("supermartijn642configlib", "sync_packet"),
+                    ConfigLib::handleSyncConfigPacket,
+                    (p, c) -> {}
+                )
+        );
     }
 
     public static boolean isClientEnvironment(){
@@ -111,12 +108,11 @@ public class ConfigLib {
     }
 
     private static void sendSyncConfigPackets(ServerPlayer sender){
-        for(ModConfig<?> config : SYNCABLE_CONFIGS){
-            channel.send(PacketDistributor.PLAYER.with(() -> sender), new ConfigSyncPacket(config));
-        }
+        for(ModConfig<?> config : SYNCABLE_CONFIGS)
+            PacketDistributor.PLAYER.with(sender).send(new ConfigSyncPacket(config));
     }
 
-    private static void createSyncedEntriesPacket(ConfigSyncPacket packet, FriendlyByteBuf buffer){
+    protected static void writeSyncedEntriesPacket(ConfigSyncPacket packet, FriendlyByteBuf buffer){
         ModConfig<?> config = packet.config;
         buffer.writeUtf(config.getIdentifier());
         try{
--- src/main/java/com/supermartijn642/configlib/ConfigSyncPacket.java
+++ src/main/java/com/supermartijn642/configlib/ConfigSyncPacket.java
@@ -1,10 +1,15 @@
 package com.supermartijn642.configlib;
 
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+
 /**
  * Created 1/19/2021 by SuperMartijn642
  */
-public class ConfigSyncPacket {
+public class ConfigSyncPacket implements CustomPacketPayload {
 
+    public static final ResourceLocation IDENTIFIER = new ResourceLocation("supermartijn642configlib", "sync_packet");
     public final ModConfig<?> config;
 
     protected ConfigSyncPacket(ModConfig<?> config){
@@ -13,5 +18,15 @@ public class ConfigSyncPacket {
 
     public ConfigSyncPacket(){
         this(null);
+    }
+
+    @Override
+    public void write(FriendlyByteBuf buffer){
+        ConfigLib.writeSyncedEntriesPacket(this, buffer);
+    }
+
+    @Override
+    public ResourceLocation id(){
+        return IDENTIFIER;
     }
 }
