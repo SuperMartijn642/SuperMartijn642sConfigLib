72b322080d9e7f9a3b2867d89ca77bd1a3869a62
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-12-25 12:47:44 +0100

    Update to 1.21.11

--- src/main/java/com/supermartijn642/configlib/ConfigSyncPacket.java
+++ src/main/java/com/supermartijn642/configlib/ConfigSyncPacket.java
@@ -3,14 +3,14 @@ package com.supermartijn642.configlib;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 /**
  * Created 1/19/2021 by SuperMartijn642
  */
 public class ConfigSyncPacket implements CustomPacketPayload {
 
-    public static final ResourceLocation IDENTIFIER = ResourceLocation.fromNamespaceAndPath("supermartijn642configlib", "sync_packet");
+    public static final Identifier IDENTIFIER = Identifier.fromNamespaceAndPath("supermartijn642configlib", "sync_packet");
     public static final CustomPacketPayload.Type<ConfigSyncPacket> TYPE = new Type<>(IDENTIFIER);
     public static final StreamCodec<FriendlyByteBuf,ConfigSyncPacket> CODEC = StreamCodec.of(ConfigLib::writeSyncedEntriesPacket, ConfigLib::handleSyncConfigPacket);
     public final ModConfig<?> config;
